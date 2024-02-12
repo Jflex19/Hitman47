6d2e8043fe5b45d677a78c4a38bb9ce6a6d3f6f7
Jflex19 <159693483+Jflex19@users.noreply.github.com>
2024-02-12 10:30:07 +0300

    Delete src/Commands/Group directory

--- src/Commands/Group/Support.ts
+++
@@ -1,5 +0,0 @@
-!support 😎
-
-<a href="https://chat.whatsapp.com/HbQRhkPZfcnFQ0wA8bKVaT">
-  <img src="https://img.shields.io/badge/Support_Group-0a0a0a?style=for-the-badge&logo=whatsapp&logoColor=white">
-</a>
